a980f6fe355e2ae058e6ad3fac33933b992d4521
Michael Ries <michael@riesd.com>
2018-12-07 13:32:38 -0700

    fix transform not to skip array values

--- lib/kalculator/transform.rb
+++ lib/kalculator/transform.rb
@@ -2,13 +2,18 @@ class Kalculator
   module Transform
     def self.run(node, &block)
       new_node = yield node
-      return new_node if is_terminal?(new_node)
-      args = new_node[1..-1].map{ |arg| run(arg, &block) }
-      args.unshift(new_node.first)
+      if is_terminal?(new_node)
+        new_node
+      elsif new_node.first.is_a?(Symbol)
+        args = new_node[1..-1].map{ |arg| run(arg, &block) }
+        args.unshift(new_node.first)
+      else
+        new_node.map{|node| run(node, &block) }
+      end
     end
 
     def self.is_terminal?(node)
-      return false if node.is_a?(Array) && node.first.is_a?(Symbol)
+      return false if node.is_a?(Array)
       true
     end
   end
--- spec/transforms_spec.rb
+++ spec/transforms_spec.rb
@@ -4,13 +4,25 @@ RSpec.describe Kalculator::Transform do
   it "can walk through an entire AST" do
     ast = Kalculator.parse("sum(List)")
     new_ast = Kalculator::Transform.run(ast) do |node|
-      if node == [:variable, "List"]
-        [:list, [[:number, 1], [:number, 2]]]
-      else
-        node
-      end
+      next [:list, [[:number, 1], [:number, 2]]] if node == [:variable, "List"]
+      node
     end
-    expect(new_ast).to eq([:sum, [:list, [[:number, 1], [:number, 2]]]])
+    expect(new_ast).to eq([:fn_call, "sum", [[:list, [[:number, 1], [:number, 2]]]]])
+  end
+
+  it "can replace items in a list" do
+    ast = Kalculator.parse("[Foo, 5, \"hi\"]")
+    new_ast = Kalculator::Transform.run(ast) do |node|
+      next [:variable, "Bar"] if node == [:variable, "Foo"]
+      next 6 if node == 5
+      next [:string, "ohai"] if node == [:string, "hi"]
+      node
+    end
+    expect(new_ast).to eq([:list, [
+      [:variable, "Bar"],
+      [:number, 6],
+      [:string, "ohai"]
+    ]])
   end
 
   it "can replace nested nodes" do
