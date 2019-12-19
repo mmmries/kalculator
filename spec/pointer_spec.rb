a9cd859b55b624be6ce2dbba465f6a54788ed025
bennewhall <bennewhall@me.com>
2019-12-19 10:14:55 -0700

    added new specs for access and pointers

---
+++ spec/pointer_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Kalculator do
+  describe "Pointers" do
+    it "points to another variable in the data sources" do
+      data = Kalculator::DataSources.new({"a"=>Kalculator::Pointer.new("name"), "name"=>"bob"})
+      expect(Kalculator.evaluate("contains(a,\"b\")",data)).to eq(true)
+    end
+    it "points to another variable in the type_sources" do
+      types = Kalculator::TypeSources.new({"a"=>Kalculator::Pointer.new("name"), "name"=>Kalculator::String.new})
+      expect(Kalculator.validate("contains(a,\"b\")",types)).to eq(Kalculator::Bool)
+    end
+  end
+end
